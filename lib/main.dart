90e3cdfec845a8c51d6e35ff7c28da5d462657b1
FlutLab.io <>
2021-01-12 17:48:49 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+    runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget{
+
+    @override
+    State<StatefulWidget>createState(){
+        return MyAppState();
+    }//createState
+}//Statefull
+
+class MyAppState extends State<MyApp> {
+
+    var index = 0;
+
+    void answerQuestion(){
+        setState((){
+            index++;
+        });//setState
+        
+        print('Answer Chosen!! ');
+    }//answerQuestion
+
+    @override
+    Widget build(BuildContext contex) {
+        var questions = [
+            'What\'s your fav color?',
+            'What\'s your fav animal?',
+        ];//Question
+        return MaterialApp(home: Scaffold(
+            appBar: AppBar(
+                title: Text("My first App"),
+            ),//AppBar
+            body: Column( 
+                children: [
+                    Text(
+                        questions[index],
+                    ),//Text
+                    RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion ),
+                    RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion ),
+                    RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion ),
+                ], //Children
+            ), //Column
+        ), //Scaffold
+      );//MaterialApp
+    };//Build
+}//extands Stateless
